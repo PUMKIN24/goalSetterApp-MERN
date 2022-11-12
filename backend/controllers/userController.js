c8d6ef91be0e4247da56da253b8eb8fa8004ba92
PUMKIN24 <jer2ymatheww@gmail.com>
2022-11-12 06:10:59 -0800

    user routes and controllers added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,75 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+// @desc    Register new User
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler( async (req, res) => {
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields')
+    }
+
+    // Check if the user exists
+    const userExists = await User.findOne({email})
+
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already Exists')
+    }
+
+    // bcrypt the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User 
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+
+    res.json({message: 'Register User'})
+})
+
+// @desc    Authenticate a User
+// @route   POST /api/users/login
+// @access  Public
+const loginUser = asyncHandler( async (req, res) => {
+   
+    res.json({message: 'Login User'})
+})
+
+
+// @desc   Get User data
+// @route  GET /api/users/me
+// @access Public
+const getMe = asyncHandler( async (req, res) => {
+    res.json({message: 'User data display'})
+})
+
+
+
+
+
+module.exports = {
+    registerUser,
+    loginUser,
+    getMe
+}
--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose')
 
-const goalSchema = mongoose.Schema({
+const goalSchema = mongoose.Schema(
+{
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
     text : {
-        type : String,
+        type: String,
         required : [true,'Please add a text value']
     }
 },{
-    timeStamps : true,
+    timeStamps : true
 }) 
 
 module.exports = mongoose.model('Goal',goalSchema)
