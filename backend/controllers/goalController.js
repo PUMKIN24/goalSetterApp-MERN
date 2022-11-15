0a5ff6e1ad248c77617d8c6bbe6d0b02defbc9fc
PUMKIN24 <jer2ymatheww@gmail.com>
2022-11-14 19:24:43 -0800

    app ready to deploy

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -39,16 +39,16 @@ const  updateGoal = asyncHandler( async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
+    
 
     //Check for user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
     // Matching the goal setter and the user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not Authorized')
     }
@@ -69,16 +69,16 @@ const  deleteGoal = asyncHandler( async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
+   
 
     //Check for user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
     // Matching the goal setter and the user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not Authorized')
     }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Login() {
  
@@ -9,6 +14,29 @@ function Login() {
     })
     
     const { email, password } = formData
+
+    
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth)
+
+        useEffect(() => {
+            if(isError) {
+                toast.error(message)
+            }
+    
+            if(isSuccess || user) {
+                navigate('/')
+            }
+    
+            dispatch(reset())
+    
+    
+    
+        }, [user, isError, isSuccess, message, navigate, dispatch])
 
 
     const onChange = (e)=>  {
@@ -21,8 +49,18 @@ function Login() {
 
     const onSubmit = (e)=> {
         e.preventDefault()
+
+        const userData = {
+            email,
+            password
+        }
+
+        dispatch(login(userData))
     }
 
+    if(isLoading) {
+        return <Spinner />
+    }
 
     return <>
         <section className='heading' >
@@ -51,7 +89,7 @@ function Login() {
                 type="password" 
                 className='form-control' 
                 id='password' 
-                name='password' 
+                name='password'
                 value={password} 
                 placeholder='Enter your password' 
                 onChange={onChange} 
